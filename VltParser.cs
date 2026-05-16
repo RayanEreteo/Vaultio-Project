6597f4834951a09861cc99f09bc3ba0fcd6217f9
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2026-05-16 22:25:35 +0200

    Finissions de la méthode de GetKeyPairs

--- VltParser.cs
+++ VltParser.cs
@@ -1,8 +1,17 @@
 public class VltParser
 {
-    public static void GetKeyPair(string temp_path)
+    /// <summary>
+    /// Récupére toutes les pairs de clé du fichier Vlt
+    /// </summary>
+    /// <param name="temp_path">Le chemin du fichier .Vlt</param>
+    public static void GetKeyPairs(string temp_path)
     {
-        Console.WriteLine(File.ReadAllText(temp_path));
+        string[] key_pairs = File.ReadAllLines(temp_path);
 
+        for (int i = 0; i < key_pairs.Length; i++)
+        {
+            string formatted = key_pairs[i].Replace("=", " : ");
+            Console.WriteLine(i + "." + " " + formatted);
+        }
     }
 }
